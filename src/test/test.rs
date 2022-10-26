9e0c73afbf69dd3310b2661a0ebcdd08eda75b12
borngraced <samuelonoja970@gmail.com>
2022-10-26 16:37:42 +0100

    fix test

--- src/test/test.rs
+++ src/test/test.rs
@@ -1,4 +1,4 @@
-/* use luno_rust_api::Luno;
+use luno_rs::Luno;
 use serde_json::json;
 use std::env;
 
@@ -7,11 +7,8 @@ async fn test_luno_async() {
 	dotenv::dotenv().ok();
 	let key = env::var("API_KEY").expect("Api Key doesn't exist yet, please add");
 	let secret = env::var("API_SECRET").expect("Api Key Secret doesn't exist yet, please add");
-	let luno = luno_rs::Luno::init(key, secret).await;
+	let luno = Luno::init(key, secret).await;
 	let balance = luno.get_all_balance().await;
-	assert!(tickers.is_ok());
-    let all_balance: Vec<Balance> = balance.unwrap();
-    print!("{balance:#?}")
-} assert_eq!(true, tickers.is_ok());
+
+	assert!(balance.is_ok());
 }
- */
